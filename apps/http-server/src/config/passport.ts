11753fdd06e875b80dceebb06df8aac01e61ef4f
Rajkumar <thecodeheist01@gmail.com>
2026-03-31 19:25:56 +0530

    docker_error_fixed_after_two_hours

--- apps/http-server/src/config/passport.ts
+++ apps/http-server/src/config/passport.ts
@@ -1,19 +1,19 @@
 
-import { prisma } from '@repo/db';
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+// import { prisma } from '@repo/db';
+// import passport from 'passport';
+// import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID ?? "",
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    callbackURL: "/auth/google/callback"
-},
-    async (accessToken, refreshToken, profile, cb) => {
-        try {
-            let user = await prisma.user.update(where: { googleId: profile.id }, data: { isLoggedIn: true })
+// passport.use(new GoogleStrategy({
+//     clientID: process.env.GOOGLE_CLIENT_ID ?? "",
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+//     callbackURL: "/auth/google/callback"
+// },
+//     async (accessToken, refreshToken, profile, cb) => {
+//         try {
+//             let user = await prisma.user.update(where: { googleId: profile.id }, data: { isLoggedIn: true })
 
-        } catch (error) {
+//         } catch (error) {
 
-        }
-    }
-));
+//         }
+//     }
+// ));
